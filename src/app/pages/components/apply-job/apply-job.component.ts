40259d2d29a5f764d95a78df460a4b32c88fd7a8
eslamfront55 <eslamfront55@gmail.com>
2023-05-24 01:33:50 +0200

    apply-job

--- src/app/pages/components/apply-job/apply-job.component.ts
+++ src/app/pages/components/apply-job/apply-job.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./apply-job.component.scss']
 })
 export class ApplyJobComponent implements OnInit {
-
+  searchResults: any = [];
+  recommendedResults: any = [
+    { id: 1, image: '../../../../assets/images/home/product.jfif', title: 'Product Designer', name: 'Google', rate: 3, address: 'Tunis,Tunisia', time: new Date() },
+    { id: 2, image: '../../../../assets/images/home/product.jfif', title: 'Product Designer', name: 'Google', rate: 3, address: 'Tunis,Tunisia', time: new Date() },
+    { id: 3, image: '../../../../assets/images/home/product.jfif', title: 'Product Designer', name: 'Google', rate: 3, address: 'Tunis,Tunisia', time: new Date() },
+    { id: 4, image: '../../../../assets/images/home/product.jfif', title: 'Product Designer', name: 'Google', rate: 3, address: 'Tunis,Tunisia', time: new Date() },
+    { id: 5, image: '../../../../assets/images/home/product.jfif', title: 'Product Designer', name: 'Google', rate: 3, address: 'Tunis,Tunisia', time: new Date() },
+    { id: 6, image: '../../../../assets/images/home/product.jfif', title: 'Product Designer', name: 'Google', rate: 3, address: 'Tunis,Tunisia', time: new Date() },
+  ];
+  rating3: any = 3;
+  skills: any = [{ title: 'User interface Design' }, { title: 'User interface Design Experience' }, { title: '88' }]
   constructor() { }
 
   ngOnInit(): void {
