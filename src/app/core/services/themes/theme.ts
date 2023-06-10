a987a13737ac57c2ecbf35e1113925c113ad8e0b
eslamfront55 <eslamfront55@gmail.com>
2023-06-10 17:59:19 +0200

    auth theme

--- src/app/core/services/themes/theme.ts
+++ src/app/core/services/themes/theme.ts
@@ -15,7 +15,10 @@ export const light: Theme = {
     '--bg-dark-color': "#111",
     '--bg-counterDot': "#cdd3df",
     '--body-color': "#ebeef5",
-    '--grey-900': '#0000008a'
+    '--grey-900': '#0000008a',
+    '--text-form-title-color': '#3A5380',
+    '--bg-register-input-color': '#576f9333',
+    '--register-photo-color': '#9aa8be'
 
   },
 };
@@ -25,14 +28,16 @@ export const dark: Theme = {
   properties: {
     "--text-main-color": " #000",
     "--bg-main-color": " #000",
-    "--bg-white-color": "#111",
+    "--bg-white-color": "#1B2730",
     "--bg-onBoarding-color": "#06141D",
-    '--bg-input-color': "#1B2730",
+    '--bg-input-color': "#576F9333",
     '--text-dark-color': "#fff",
     '--bg-dark-color': "#fff",
     '--bg-counterDot': "#0F2656",
     '--body-color': "#06141D",
-    '--grey-900': '#ffffff8a'
-
+    '--grey-900': '#ffffff8a',
+    '--text-form-title-color': '#FFFFFF',
+    '--bg-register-input-color': '#576F9333',
+    '--register-photo-color': '#3e526b'
   }
 }
